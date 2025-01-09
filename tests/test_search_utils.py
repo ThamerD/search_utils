a48f9a50fee030032580b889aee7dbe5e6799b37
Thamer <tha-mer@hotmail.com>
2025-01-08 20:25:28 -0800

    Added test

--- tests/test_search_utils.py
+++ tests/test_search_utils.py
@@ -1,13 +1,10 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from src.search_utils.search_utils import find_all
+from search_utils.search_utils import find_all
 
-test_list = [1,2,3,1,5,6,1,1]
+def test_find_all():
+    test_list = [1,2,3,1,5,6,1,1]
 
-actual = find_all(test_list, 1)
+    actual = find_all(test_list, 1)
 
-expected = [1,1,1,1]
+    expected = [1,1,1,1]
 
-assert actual == expected
-assert 1 == 1
+    assert actual == expected, f"Expected {expected}, but got {actual}"
